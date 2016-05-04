9e7d5f50644949ed37ca8ac5543436c9fa95399c
Gil Mishal <gilmishal@gmail.com>
2016-05-04 20:51:37 +0300

    fixed for angular2 rc

--- SlydeWeb/app/components/app.component.ts
+++ SlydeWeb/app/components/app.component.ts
@@ -1,7 +1,7 @@
-﻿import {Component} from 'angular2/core';
+﻿import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent { }   
+export class AppComponent { }
